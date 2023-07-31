abd99c6da5093c7643d5220b1d643ddc1e3e267a
Med-hat2023 <medhat.dheif@gmail.com>
2023-07-31 14:50:48 -0700

    6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -8,14 +8,14 @@
 */
 int pop_listint(listint_t **head)
 {
-listint_t *temp;
-int num;
+listint_t *tepp;
+int nim;
 if (!head || !*head)
 return (0);
-num = (*head)->n;
-temp = (*head)->next;
+nim = (*head)->n;
+tepp = (*head)->next;
 free(*head);
-*head = temp;
-return (num);
+*head = tepp;
+return (nim);
 }
 
