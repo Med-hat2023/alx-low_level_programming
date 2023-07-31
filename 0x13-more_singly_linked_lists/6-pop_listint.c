3c96761ca176978dc9c9465c6c970f3c2d871599
Med-hat2023 <medhat.dheif@gmail.com>
2023-07-31 14:48:12 -0700

    6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,24 +1,21 @@
 #include "lists.h"
 
 /**
- *pop_listint - deletess A head node a link list lisT
- *@head: the head of A list
- *::::::list
- *Return: the delete A node Of data
- */
-
+*pop_listint - deletess A head node a link list lisT
+*@head: the head of A list
+*Prototype: int pop_listint(listint_t **head);
+*Return: the delete A node Of data
+*/
 int pop_listint(listint_t **head)
 {
-	listint_t *popped;
-	int mEd;
-
-	if (*head == NULL)
-		return (0);
-
-	popped = *head;
-	mEd = popped->n;
-	free(popped);
-
-	*head = (*head)->next;
-	return (mEd);
+listint_t *temp;
+int num;
+if (!head || !*head)
+return (0);
+num = (*head)->n;
+temp = (*head)->next;
+free(*head);
+*head = temp;
+return (num);
 }
+
