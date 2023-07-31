d7169c0c80b9ad8da728d36d06c1b75000a5d082
Med-hat2023 <medhat.dheif@gmail.com>
2023-07-31 15:25:32 -0700

    100-reverse_listint.c

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - reverses A link list LiSt
+ * @head: mEan head of eth A list
+ * Prototype: listint_t *reverse_listint(listint_t **head);
+ * You are not allowed to use more than 1 loop.
+ * You are not allowed to use malloc, free or arrays
+ * You can only declare a maximum of two variables in your function
+ * Return: first node of Eth reversed A nodeS
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *next_node = NULL;
+	listint_t *prev_node = NULL;
+
+	if (head == NULL)
+		return (NULL);
+	while (*head != NULL)
+	{
+		next_node = (*head)->next;
+		(*head)->next = prev_node;
+		prev_node = *head;
+		*head = next_node;
+	}
+	*head = prev_node;
+	return (*head);
+}
