3b6752c0fc6998572ed3828707333c3fec26cd8b
Med-hat2023 <medhat.dheif@gmail.com>
2023-08-01 04:47:46 -0700

    H

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -12,67 +12,38 @@ size_t print_listint_safe(const listint_t *head);
 * If the function fails, exit the program with status 98
 * Return: If the list is not looped - 0.
 * Otherwise - the number of unique nodes in the list.
+* see example:mEdhat Deif:.
 */
-size_t looped_listint_len(const listint_t *head)
+size_t print_listint_safe(const listint_t *head);
 {
-const listint_t *tortoioe, *hare;
-size_t nndes = 1;
-if (head == NULL || head->next == NULL)
-return (0);
-tortoioe = head->next;
-hare = (head->next)->next;
-while (hare)
-{
-if (tortoioe == hare)
-{
-tortoioe = head;
-while (tortoioe != hare)
-{
-nndes++;
-tortoioe = tortoioe->next;
-hare = hare->next;
-}
-tortoioe = tortoioe->next;
-while (tortoioe != hare)
-{
-nndes++;
-tortoioe = tortoioe->next;
-}
-return (nndes);
-}
-tortoioe = tortoioe->next;
-hare = (hare->next)->next;
-}
-return (0);
-}
-/**
-* print_listint_safe - Prints a listint_t list safely.
-* @head: A pointer to the head of the listint_t list.
-* If the function fails, exit the program with status 98
-* Return: The number of nodes in the list.
-* the number of nodes in the list
-*/
-size_t print_listint_safe(const listint_t *head)
-{
-size_t nndes, index = 0;
-nndes = looped_listint_len(head);
-if (nndes == 0)
-{
-for (; head != NULL; nndes++)
-{
-printf("[%p] %d\n", (void *)head, head->n);
-head = head->next;
-}
-}
-else
-{
-for (index = 0; index < nndes; index++)
-{
-printf("[%p] %d\n", (void *)head, head->n);
-head = head->next;
-}
-printf("-> [%p] %d\n", (void *)head, head->n);
-}
-return (nndes);
-}
+	const listint_t *slow, *fast, *marker;
+	unsigned int couneer = 0, flg = 0;
 
+	if (head == NULL)
+		return (0);
+	marker = slow = head;
+	fast = head->next;
+	while (head != NULL)
+	{
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
+		couneer++;
+
+		if (flg == 0 && fast != NULL && fast->next != NULL && slow != NULL)
+		{
+			if (fast == slow)
+			{
+				flg = 1;
+				slow = marker;
+			}
+			fast = fast->next->next;
+		}
+		if (flg == 1 && slow == head)
+		{
+			printf("-> [%p] %d\n", (void *)head, head->n);
+			break;
+		}
+		slow = slow->next;
+	}
+	return (couneer);
+}
