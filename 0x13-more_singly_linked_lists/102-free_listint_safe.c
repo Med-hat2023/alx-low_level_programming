831db12e1314b51aca8dba6d4a379bd10c28aa86
Med-hat2023 <medhat.dheif@gmail.com>
2023-08-01 06:22:19 -0700

    I

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -5,27 +5,38 @@
  * @h: address of pointer to first node
  * This function can though the list only once
  * The function sets the head to NULL:Medhat Deif
- * Return: address of head
+ * Return: the the address of head medhat Deif::
  */
 size_t free_listint_safe(listint_t **h)
 {
-	size_t f = 0;
-	listint_t *head, *tem;
-	long diff;
+	size_t len = 0;
+	int diff;
+	listint_t *tem;
 
-	if (!h)
+	if (!h || !*h)
 		return (0);
-	head = *h;
-	*h = NULL;
-	while (head)
+
+	while (*h)
 	{
-		f++;
-		diff = head->next - head;
-		tem = head;
-		free(tem);
-		if (diff >= 0)
+		diff = *h - (*h)->next;
+		if (diff > 0)
+		{
+			tem = (*h)->next;
+			free(*h);
+			*h = tem;
+			len++;
+		}
+		else
+		{
+			free(*h);
+			*h = NULL;
+			len++;
 			break;
-		head = head->next;
+		}
 	}
-	return (f);
+
+	*h = NULL;
+
+	return (len);
 }
+
