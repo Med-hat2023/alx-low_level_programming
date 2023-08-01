f5202d2872ddbe1de999174e7b51b47873b85289
Med-hat2023 <medhat.dheif@gmail.com>
2023-08-01 03:05:05 -0700

    Z

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -3,29 +3,33 @@
 /**
  * get_nodeint_at_index - returns eth nth node of the link list
  * @head: Pointer to head of eth list lIst
- * @index: index of the node required
- * index is the index of the node, starting at 0
+ * @index: index of the node Required TO medhat dEif::
+ * index is the index of the Node, starting at 0
  * Return: the address of eth nods
+ * if the node does not exist, return NULL
+ * where index is the index of the node, starting at 0::
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *cursor = NULL;
-	unsigned int n = 0;
+	unsigned int nodes = 0;
+	listint_t *copo;
+	unsigned int couneer = 0;
 
-	while (head != NULL)
+	copo = head;
+
+	while (copo != NULL)
 	{
-		if (n <= index)
-		{
-			if (n == index)
-			{
-				cursor = head;
-				break;
-			}
-			head = head->next;
-			n++;
-		}
-		else
-			return (NULL);
+		nodes++;
+		copo = copo->next;
 	}
-	return (cursor);
+	copo = head;
+	if (index > nodes - 1)
+		return (NULL);
+
+	while (couneer != index)
+	{
+		couneer++;
+		copo = copo->next;
+	}
+	return (copo);
 }
